e3e207f3492a1e4d5167e2bd914a1ee8b872bc33
Eduardo Gonzalez Briceno <eduardoegb@gmail.com>
2023-06-08 10:25:13 -0400

    Added current buy in match

--- Player.js
+++ Player.js
@@ -8,11 +8,8 @@ class Player {
     const bigBlind = small_blind * 2;
     const betLimit = bigBlind * 2;
     
-    if (current_buy_in <= betLimit) {
-      bet(current_buy_in);
-    }
-    
-    bet(0);
+    bet(current_buy_in);
+
   }
 
   static showdown(gameState) {
