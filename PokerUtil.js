6d8e2822b7515647d17125fb9fd8e43b1198999f
Hoi Tik Wong <wong@rushstreetinteractive.com>
2023-06-08 11:24:25 -0400

    should check card1 here

--- PokerUtil.js
+++ PokerUtil.js
@@ -58,7 +58,7 @@ class PokerUtils {
         || (card1.rank === 'Q' && this._rankGreaterThan(card2.rank, 9))) {
         return 3;
       } else if ((card1.rank === 'A' && card2.rank === '2')
-      || (card1.rank === 'K' || card1.rank ==='Q' || card2.rank ==='J')
+      || (card1.rank === 'K' || card1.rank ==='Q' || card1.rank ==='J')
       || (card1.rank === '10' && this._rankGreaterThan(card2.rank, 5))
       || (card1.rank === '9' && this._rankGreaterThan(card2.rank, 5))
       || (card1.rank === '8' && card2.rank === '7')) {
